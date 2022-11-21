d02b032a3938c76a47aba7144be59b8ba2de3836
Annielee-cyberforce <108461361+Annielee-cyberforce@users.noreply.github.com>
2022-11-21 17:43:07 +0800

    Update paloaltocortexxdr_connector.py

--- paloaltocortexxdr_connector.py
+++ paloaltocortexxdr_connector.py
@@ -58,17 +58,17 @@ class TestConnector(BaseConnector):
                     error_code = e.args[0]
                     error_msg = e.args[1]
                 elif len(e.args) == 1:
-                    error_code = ERR_CODE_MSG
+                    error_code = PA_ERR_CODE_MSG
                     error_msg = e.args[0]
             else:
-                error_code = ERR_CODE_MSG
-                error_msg = ERR_MSG_UNAVAILABLE
+                error_code = PA_ERR_CODE_MSG
+                error_msg = PA_ERR_MSG_UNAVAILABLE
         except:
-            error_code = ERR_CODE_MSG
-            error_msg = ERR_MSG_UNAVAILABLE
+            error_code = PA_ERR_CODE_MSG
+            error_msg = PA_ERR_MSG_UNAVAILABLE
 
         try:
-            if error_code in ERR_CODE_MSG:
+            if error_code in PA_ERR_CODE_MSG:
                 error_text = "Error Message: {0}".format(error_msg)
             else:
                 error_text = "Error Code: {0}. Error Message: {1}".format(error_code, error_msg)
